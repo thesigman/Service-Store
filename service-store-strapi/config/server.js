5fb27da4b44fbf22051796b66f91c0e98cd7084f
Thanos Kapsalis <thanos@ep-mail.gr>
2021-07-23 19:39:19 +0300

    Graphics only

--- service-store-strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'ef88554fda96d14b4aac62461a49f61b'),
-    },
-  },
-});
--- service-store-strapi/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '36e5f2d9-4a4e-4a6e-ab41-b97d19f081fa'
-};

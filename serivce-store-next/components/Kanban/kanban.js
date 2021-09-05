299795f3c3edf062a8fe181eef638e2d014c8795
Thanos Kapsalis <thanoskaspalis97@gmail.com>
2021-09-05 15:25:32 +0300

    Πρσοθήκη kanban js

---
+++ serivce-store-next/components/Kanban/kanban.js
@@ -0,0 +1,88 @@
+import React from "react";
+import Board from 'react-trello';
+import styles from '../Card/card.module.scss'
+
+
+export default function Kanban(props) {
+  // Κατασκευή του board με βάση τα δεδομένα που θέλουμε να φαίνονται
+  let taskCard = {
+    'border-radius': '6px',
+    'box-shadow': 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)'
+  }
+  let cardStyle = {
+    'background-color': ' #ffff',
+    'box-shadow': '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    'border-radius': '6px',
+
+  }
+  const data = {
+    lanes: [
+      {
+        id: 'todo',
+        title: 'To Do',
+        cardStyle : cardStyle,
+        style: {
+          'border-radius': '6px',
+          'box-shadow': 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)',
+          'backgroundColor': '#607890',
+          'color': 'white',
+          'font-size': '28px',
+          'width': '25%',
+        },
+
+        cards: [
+          { id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: true },
+          { id: 'Card3', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: { sha: 'be312a1' } },
+          { id: 'Card4', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: { sha: 'be312a1' } },
+          { id: 'Card5', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: { sha: 'be312a1' } },
+          { id: 'Card6', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: { sha: 'be312a1' } },
+        ]
+      },
+      {
+        id: 'inprogress',
+        cardStyle : cardStyle,
+        style: {
+          'border-radius': '6px',
+          'box-shadow': 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)',
+          'backgroundColor': '#FFE66D',
+          'color': '#666666',
+          'font-size': '28px',
+          'width': '25%',
+        },
+        title: 'Completed',
+        cards: []
+      },
+      {
+        id: 'done',
+        cardStyle : cardStyle,
+        style: {
+          'border-radius': '6px',
+          'box-shadow': 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)',
+          'backgroundColor': '#5FA052',
+          'color': 'white',
+          'font-size': '28px',
+          'width': '25%',
+        },
+        title: 'Done',
+        cards: []
+      },
+      {
+        id: 'messages',
+        cardStyle : cardStyle,
+        style: {
+          'border-radius': '6px',
+          'box-shadow': 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)',
+          'backgroundColor': '#666666',
+          'color': 'white',
+          'font-size': '58px'
+        },
+        title: 'Messages',
+        label: '0/0',
+        cards: []
+      },
+    ]
+  }
+
+
+  return (<Board editable={true} style={{ 'backgroundColor': 'transparent' }} data={data}></Board>);
+}
--- serivce-store-next/pages/home.js
+++ serivce-store-next/pages/home.js
@@ -8,76 +8,20 @@ import TaskCard from '../components/Card/taskCard';
 import AdminBar from '../components/AdminBar/adminBar'
 import Layout from '../components/Layout/layout'
 import { useState, useEffect } from 'react'
+import Kanban from '../components/Kanban/kanban'
+
 
 export default function Home(props) {
 
-  /**
-   * Το state και το effect θα χρησιμοποιηθο΄ύν για την οργάνωση της πληροφορίας
-   * Ωστόσο η πληροφορία αρχικά θα οργανωθεί σε πίνακες προκειμένου να ολοκληρωθούν οι εργασίες
-   * ανεξάρτητα της πορείας του api
-   */
-  const [toDOsCount, setTODos] = useState();
-  const [inProgressCount, setInProgress] = useState();
-  const [doneCount, setDone] = useState();
-  const [messagesCount, setMessages] = useState();
-
-  useEffect(() => {
-    /**
-     * Τρέχει όταν φορτώνει η σελίδα εδώ θα γίνει το request με την βάση
-     */
-    setTODos(8);
-    setInProgress(3);
-    setDone(4);
-    setMessages(5);
-
-  }, []);
-
-  /**
-   * TaskCard αποτελείτ την μεγάλη κάρτα που αναπαριστά κάθε κατηγορία 
-   * Η κάρτα για να εμφανιστεί θα πρέπει να ξέρει τον τιτλο το χρώμα
-   * Ο αριθμός των στοιχείων χρησιμεύει στο πόσα στοιχεία θα πρέπει να ρεντάρει
-   * και ο τύπος στο να γνωρίζει ποιο endpoint θα πρέπει να καλέσει
-   */
 
   return (
     <Layout user={props}>
       {!props.isAuthenticated && <p>You have to login First</p> ||
         <div className="row">
-          <div className="col-3">
-            <TaskCard
-              title="To Do"
-              cardBg="bg-primary"
-              counter={toDOsCount}
-              type="todo"
-            >
-            </TaskCard>
-          </div>
-          <div className="col-3">
-            <TaskCard
-              title="In progess"
-              cardBg="bg-secondary"
-              counter={inProgressCount}
-              type="progress"
-            ></TaskCard>
-          </div>
-          <div className="col-3">
-            <TaskCard
-              title="Done"
-              cardBg="bg-success"
-              counter={doneCount}
-              type="done"
-            ></TaskCard>
-          </div>
-          <div className="col-3">
-            <TaskCard
-              title="Messages"
-              cardBg="bg-gray"
-              counter={messagesCount}
-              type="message"
-            ></TaskCard>
-          </div>
+          <Kanban></Kanban>
         </div>
       }
+
     </Layout>
 
   )
